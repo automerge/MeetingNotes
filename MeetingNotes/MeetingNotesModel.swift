4c6fdcf2dac623ce5bd1d58c37efa4c79add9c2c
Joseph Heck <heckj@mac.com>
2023-06-26 11:55:09 -0700

    docs: adding internal docs and schema notes (#5)

--- MeetingNotes/MeetingNotesModel.swift
+++ MeetingNotes/MeetingNotesModel.swift
@@ -1,30 +1,31 @@
 import Automerge
 import Foundation
-#if os(iOS)
-import UIKit
-#elseif os(macOS)
-import AppKit
-#endif
 
-// What I'd like to have as my "meeting notes" schema:
-//
-// - ROOT {
-//     "id" - UUID (Scalar) // for comparing origins for sync
-//     "title" - String (scalar)
-//     "attendees" - [ String (scalar) ]
-//     "agenda" - [
-//        AgendaItem {
-//           "title": String (scalar)
-//           "discussion": Text
-//        }
-//     ]
-//   }
-
+/// An individual agenda item tracked by meeting notes.
+/// The `discussion` property is the type `Text` is from Automerge, and represents a collaboratively edited string.
 struct AgendaItem: Codable {
     var title: String
     var discussion: Text
 }
 
+/// The top-level application model for Meeting Notes.
+///
+/// The `id` is meant to provide a root document identifier for use in comparing synced or external documents to determine if they share a common history.
+///
+/// The overall document schema is laid out to map into Automerge using the following schema:
+/// ```
+/// ROOT {
+///   "id" - UUID (as a scalar value)
+///   "title" - String (as a scalar value)
+///   "attendees" - [ String (as a scalar value) ]
+///   "agenda" - [
+///      AgendaItem {
+///        "title": String (as a scalar value)
+///        "discussion": Text (collaborative sync for edits)
+///      }
+///   ]
+/// }
+/// ```
 struct MeetingNotesModel: Codable, Identifiable {
     let id: UUID
     var title: String
