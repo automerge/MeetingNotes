19f78ad284dd7328841da144481cc9dcc079fd3d
Joseph Heck <heckj@mac.com>
2023-06-28 10:05:25 -0700

    resetting example type identifier (#7)

--- MeetingNotes/ContentView.swift
+++ MeetingNotes/ContentView.swift
@@ -11,9 +11,23 @@ struct ContentView: View {
                     .font(.caption)
                 Spacer()
             }
-            Form {
-                TextField("Title", text: $document.model.title)
+            List {
+                Section {
+                    TextField("Title", text: $document.model.title)
+                }
+                Section("Attendees") {
+                    ForEach(document.model.attendees, id: \.self) { attendee in
+                        Text(attendee)
+                    }
+                }
+                Section("Agenda") {
+                    ForEach(document.model.agenda, id: \.self) { agendaItem in
+                        Text(agendaItem.title)
+                    }
+                }
             }
+            
+            
         }
     }
 }
