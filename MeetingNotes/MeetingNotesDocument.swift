f2919bf1462adece054bfef9fd175246cc7757d3
Joe Heck <heckj@mac.com>
2023-06-28 10:26:17 -0700

    fixing self references, stripped by swiftformat

--- MeetingNotes/MeetingNotesDocument.swift
+++ MeetingNotes/MeetingNotesDocument.swift
@@ -97,7 +97,7 @@ final class MeetingNotesDocument: ReferenceFileDocument {
         if model.id != wrappedDocument.id {
             logger
                 .error(
-                    "Internal document id: \(model.id, privacy: .public) doesn't match the origin ID in the file wrapper (\(wrappedDocument.id, privacy: .public)"
+                    "Internal document id: \(self.model.id, privacy: .public) doesn't match the origin ID in the file wrapper (\(wrappedDocument.id, privacy: .public)"
                 )
             throw CocoaError(.fileReadCorruptFile)
         }
