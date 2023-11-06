28d86fe3b34a1f1e294453bc9eb5f91b8fd68aca
Joe Heck <heckj@mac.com>
2023-11-06 13:02:41 -0800

    starting to dig into changeover of DocumentID to use bs58 for canonical encoding

--- MeetingNotesTests/MeetingNotesTests.swift
+++ MeetingNotesTests/MeetingNotesTests.swift
@@ -1,20 +1,50 @@
+import Base58Swift
+import Foundation
 import XCTest
+
+extension UUID {
+    var data: Data {
+        var byteblob = Data(count: 16)
+        byteblob[0] = self.uuid.0
+        byteblob[1] = self.uuid.1
+        byteblob[2] = self.uuid.2
+        byteblob[3] = self.uuid.3
+        byteblob[4] = self.uuid.4
+        byteblob[5] = self.uuid.5
+        byteblob[6] = self.uuid.6
+        byteblob[7] = self.uuid.7
+        byteblob[8] = self.uuid.8
+        byteblob[9] = self.uuid.9
+        byteblob[10] = self.uuid.10
+        byteblob[11] = self.uuid.11
+        byteblob[12] = self.uuid.12
+        byteblob[13] = self.uuid.13
+        byteblob[14] = self.uuid.14
+        byteblob[15] = self.uuid.15
+        return byteblob
+    }
+}
 
 final class MeetingNotesTests: XCTestCase {
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions
-        // afterwards.
+    func testFiddlingWithBase58() throws {
+        let exampleUUID = UUID()
+        let bytes: Data = exampleUUID.data
+        print(bytes.count)
+
+        // let full = "automerge:2j9knpCseyhnK8izDmLpGP5WMdZQ"
+        let partial = "2j9knpCseyhnK8izDmLpGP5WMdZQ"
+        if let decodedBytes = Base58.base58CheckDecode(partial) {
+            print(decodedBytes.count)
+            // both are 16 bytes of data
+            XCTAssertEqual(bytes.count, Data(decodedBytes).count)
+        }
     }
 }
