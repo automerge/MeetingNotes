410e0fa4e5d681192a22cf03d4dff9c3ea15aaa3
Joe Heck <heckj@mac.com>
2024-01-29 12:10:54 -0800

    expanded websocket message structures from CDDL in (updated) protocol document

--- MeetingNotes/WebSocketNetworking/WebSocketMessages.swift
+++ MeetingNotes/WebSocketNetworking/WebSocketMessages.swift
@@ -9,35 +9,46 @@ import Foundation
 
 // related source for the automerge-repo sync code:
 // https://github.com/automerge/automerge-repo/blob/main/packages/automerge-repo-network-websocket/src/BrowserWebSocketClientAdapter.ts
-// It looks like all websocket messages are CBOR encoded as data streams
-//
-// 1. So first step is getting through the handshake, then we can find and sort
-// the sync messages that flow back and forth.
+// All the websocket messages are CBOR encoded and sent as data streams
 
 // CDDL pre-amble
 // ; The base64 encoded bytes of a Peer ID
 // peer_id = str
+// ; The base64 encoded bytes of a Storage ID
+// storage_id = str
 // ; The possible protocol versions (currently always the string "1")
 // protocol_version = "1"
 // ; The bytes of an automerge sync message
 // sync_message = bstr
 // ; The base58check encoded bytes of a document ID
 // document_id = str
+// ; Metadata sent in either the join or peer message types
+// peer_metadata = {
+//    ; The storage ID of this peer
+//    ? storageId: storage_id,
+//    ; Whether the sender expects to connect again with this storage ID
+//    isEphemeral: bool
+// }
 //
 // - join -
 // {
 //    type: "join",
 //    senderId: peer_id,
 //    supportedProtocolVersions: protocol_version
+//    ? metadata: peer_metadata,
 // }
 
 struct JoinMsg: Codable {
     var type: String = "join"
     let senderId: String
     var supportedProtocolVersions: String = "1"
+    var peerMetadata: PeerMetadata?
 
-    init(senderId: String) {
+    init(senderId: String, metadata: PeerMetadata? = nil) {
         self.senderId = senderId
+        if let metadata {
+            self.peerMetadata = metadata
+        }
     }
 }
 
@@ -47,8 +58,10 @@ struct JoinMsg: Codable {
 //    senderId: peer_id,
 //    selectedProtocolVersion: protocol_version,
 //    targetId: peer_id,
+//    ? metadata: peer_metadata,
 // }
 
+// example output from sync.automerge.org:
 // {
 //   "type": "peer",
 //   "senderId": "storage-server-sync-automerge-org",
@@ -57,16 +70,23 @@ struct JoinMsg: Codable {
 //   "targetId": "FA38A1B2-1433-49E7-8C3C-5F63C117DF09"
 // }
 
+struct PeerMetadata: Codable {
+    var storageId: String?
+    var isEphemeral: Bool
+}
+
 struct PeerMsg: Codable {
     var type: String = "peer"
     let senderId: String
     let targetId: String
+    var peerMetadata: PeerMetadata?
     var selectedProtocolVersion: String
 
-    init(senderId: String, targetId: String) {
+    init(senderId: String, targetId: String, storageId: String?, ephemeral: Bool = true) {
         self.senderId = senderId
         self.targetId = targetId
         self.selectedProtocolVersion = "1"
+        self.peerMetadata = PeerMetadata(storageId: storageId, isEphemeral: ephemeral)
     }
 }
 
@@ -84,3 +104,147 @@ struct ErrorMsg: Codable {
         self.message = message
     }
 }
+
+// - request -
+// {
+//    type: "request",
+//    documentId: document_id,
+//    ; The peer requesting to begin sync
+//    senderId: peer_id,
+//    targetId: peer_id,
+//    ; The initial automerge sync message from the sender
+//    data: sync_message
+// }
+
+struct RequestMsg: Codable {
+    var type: String = "error"
+    let documentId: String
+    let senderId: String // The peer requesting to begin sync
+    let targetId: String
+    let sync_message: Data // The initial automerge sync message from the sender
+
+    init(documentId: String, senderId: String, targetId: String, sync_message: Data) {
+        self.documentId = documentId
+        self.senderId = senderId
+        self.targetId = targetId
+        self.sync_message = sync_message
+    }
+}
+
+// - sync -
+// {
+//    type: "sync",
+//    documentId: document_id,
+//    ; The peer requesting to begin sync
+//    senderId: peer_id,
+//    targetId: peer_id,
+//    ; The initial automerge sync message from the sender
+//    data: sync_message
+// }
+
+struct SyncMsg: Codable {
+    var type = "sync"
+    let documentId: String
+    let senderId: String // The peer requesting to begin sync
+    let targetId: String
+    let data: Data // The initial automerge sync message from the sender
+
+    init(documentId: String, senderId: String, targetId: String, data: Data) {
+        self.documentId = documentId
+        self.senderId = senderId
+        self.targetId = targetId
+        self.data = data
+    }
+}
+
+// - unavailable -
+// {
+//  type: "doc-unavailable",
+//  senderId: peer_id,
+//  targetId: peer_id,
+//  documentId: document_id,
+// }
+
+struct UnavailableMsg: Codable {
+    var type = "doc-unavailable"
+    let documentId: String
+    let senderId: String
+    let targetId: String
+
+    init(documentId: String, senderId: String, targetId: String) {
+        self.documentId = documentId
+        self.senderId = senderId
+        self.targetId = targetId
+    }
+}
+
+// - ephemeral -
+// {
+//  type: "ephemeral",
+//  ; The peer who sent this message
+//  senderId: peer_id,
+//  ; The target of this message
+//  targetId: peer_id,
+//  ; The sequence number of this message within its session
+//  count: uint,
+//  ; The unique session identifying this stream of ephemeral messages
+//  sessionId: str,
+//  ; The document ID this ephemera relates to
+//  documentId: document_id,
+//  ; The data of this message (in practice this is arbitrary CBOR)
+//  data: bstr
+// }
+
+struct EphemeralMsg: Codable {
+    var type = "ephemeral"
+    let senderId: String
+    let targetId: String
+    let count: UInt
+    let sessionId: String
+    let documentId: String
+    let data: Data
+
+    init(senderId: String, targetId: String, count: UInt, sessionId: String, documentId: String, data: Data) {
+        self.senderId = senderId
+        self.targetId = targetId
+        self.count = count
+        self.sessionId = sessionId
+        self.documentId = documentId
+        self.data = data
+    }
+}
+
+// - remote subscription changed -
+// {
+//  type: "remote-subscription-change"
+//  senderId: peer_id
+//  targetId: peer_id
+//
+//  ; The storage IDs to add to the subscription
+//  ? add: [* storage_id]
+//
+//  ; The storage IDs to remove from the subscription
+//  remove: [* storage_id]
+// }
+
+// - remote heads changed
+// {
+//  type: "remote-heads-changed"
+//  senderId: peer_id
+//  targetId: peer_id
+//
+//  ; The document ID of the document that has changed
+//  documentId: document_id
+//
+//  ; A map from storage ID to the heads advertised for a given storage ID
+//  newHeads: {
+//    * storage_id => {
+//      ; The heads of the new document for the given storage ID as
+//      ; a list of base64 encoded SHA2 hashes
+//      heads: [* string]
+//      ; The local time on the node which initially sent the remote-heads-changed
+//      ; message as milliseconds since the unix epoch
+//      timestamp: uint
+//    }
+//  }
+// }
