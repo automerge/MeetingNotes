604b68a0334a222417bdbf05ebb82fca16c3aa92
Joe Heck <heckj@mac.com>
2024-03-09 13:07:37 -0800

    working on concurrency warnings in Swift 5.10

--- MeetingNotes/Logger+extensions.swift
+++ MeetingNotes/Logger+extensions.swift
@@ -1,7 +1,11 @@
 import Foundation
 import OSLog
 
-extension Logger {
+// using : @unchecked Sendable here because I think Logger _is_ sendable,
+// but isn't yet marked as such. Alternatively I think we could use @preconcurrency import,
+// but doing this due to the conversation on the forums (Mar2024):
+// https://forums.swift.org/t/preconcurrency-doesnt-suppress-static-property-concurrency-warnings/70469/2
+extension Logger: @unchecked Sendable {
     /// Using your bundle identifier is a great way to ensure a unique identifier.
     nonisolated private static let subsystem = Bundle.main.bundleIdentifier!
 
