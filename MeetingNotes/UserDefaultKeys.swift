19d550124d66e9c9efe9195393090b061876ef7f
Joe Heck <heckj@mac.com>
2024-04-15 11:16:40 -0700

    moving userdefault usage into App from library, need to filter self-peers from syncview

---
+++ MeetingNotes/UserDefaultKeys.swift
@@ -0,0 +1,5 @@
+/// A collection of User Default keys for the app.
+public enum UserDefaultKeys: Sendable {
+    /// The key to the string that the app broadcasts to represent you when sharing and syncing Automerge Documents.
+    public static let publicPeerName = "sharingIdentity"
+}
